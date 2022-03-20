9a4ba645557d39e8a657dbe69e6fa8c532327d61
Laurent BOSSAVIT <216452+Morendil@users.noreply.github.com>
2022-03-20 20:54:37 +0100

    A mi-parcours

--- DriverSpec.hs
+++ DriverSpec.hs
@@ -1,12 +1,13 @@
 import Test.Hspec
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 type Routes = [Route]
 type Route = [Station]
-type Station = Int 
+type Station = Int
 type Solution = Int
 type Driver = Int
-type Knowledge = M.Map Driver [Driver]
+type Knowledge = M.Map Driver (S.Set Driver)
 
 main = hspec $ do
     describe "The number of stops it takes for all drivers to be up to date" $
@@ -15,10 +16,10 @@ main = hspec $ do
     describe "The initial state of knowledge of drivers" $ do
         it "should assign, to a lone driver, only their own gossip" $ do
             let firstDriver = 0
-            initial [[1]] `shouldBe` M.fromList [(firstDriver, [firstDriver])]                
+            initial [[1]] `shouldBe` M.fromList [(firstDriver, S.fromList [firstDriver])]
         it "should assign, to each driver, their own gossip " $ do
             let irrelevant = []
-            initial [irrelevant,irrelevant] `shouldBe` M.fromList [(driver, [driver]) | driver <- [0..1]]
+            initial [irrelevant,irrelevant] `shouldBe` M.fromList [(driver, S.fromList [driver]) | driver <- [0..1]]
     describe "A state of knowledge of drivers" $ do
         describe "should be complete when all drivers share all gossips" $ do
             it "which in the case of one driver is right away" $ do
@@ -28,21 +29,35 @@ main = hspec $ do
                 complete (initial [irrelevant,irrelevant]) `shouldBe` False
             it "which can be the case for two drivers" $ do
                 let [firstDriver, secondDriver] = [0,1]
-                    allTwoGossips = [firstDriver, secondDriver]
-                complete (M.fromList [(firstDriver,allTwoGossips),(secondDriver,allTwoGossips)]) `shouldBe` True
+                    allTwoGossips = S.fromList [firstDriver, secondDriver]
+                    completeKnowledge = M.fromList [(firstDriver,allTwoGossips),(secondDriver,allTwoGossips)]
+                complete completeKnowledge `shouldBe` True
+    describe "The evolution of the state of knowledge" $ do
+        it "takes an initial knowledge and shares gossip among two drivers" $ do
+            let [firstDriver, secondDriver] = [0,1]
+                irrelevant = []
+                startingKnowledge = initial [irrelevant, irrelevant]
+                allTwoGossips = S.fromList [firstDriver, secondDriver]
+                completeKnowledge = M.fromList [(firstDriver,allTwoGossips),(secondDriver,allTwoGossips)]
+            shareGossip startingKnowledge firstDriver secondDriver `shouldBe` completeKnowledge
 
 gossip :: Routes -> Solution
 gossip routes | complete (initial routes) = 0
               | otherwise = undefined
 
 initial :: Routes -> Knowledge
-initial routes = M.fromList [(driver, [driver]) | driver <- [0..length routes-1]]
+initial routes = M.fromList [(driver, S.fromList [driver]) | driver <- [0..length routes-1]]
 
 complete :: Knowledge -> Bool
-complete knowledge = knowledge == initial [[1]]
+complete knowledge = all (allGossips ==) $ M.elems knowledge
+    where allGossips = S.fromList $ M.keys knowledge
+
+shareGossip :: Knowledge -> Driver -> Driver -> Knowledge
+shareGossip k d1 d2 = M.insert d1 sharedGossip $ M.insert d2 sharedGossip k 
+    where allTwoGossips = S.fromList [d1, d2]
+          Just sharedGossip = S.union <$> M.lookup d1 k <*> M.lookup d2 k
 
 -- Where are we going ?
--- - use complete to implement gossip
 -- - evolve the state of knowledge
--- - remove the fake from complete
 -- - zip cycle the routes
+-- L wants to use QuickCheck but was thwarted
